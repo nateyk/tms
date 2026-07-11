5c528c54c50f0a892d3f0e48d5497ac23a30bb92
Nathanael Yitbarek <natenaelny@gmail.com>
2026-07-11 02:47:15 -0700

    Add API routes for vehicle and trailer tyre map data

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\TyreLookupController;
+use App\Http\Controllers\Tyres\TyreReadingMonitoringController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::get('/tyres/{tyre_code}', [TyreLookupController::class, 'show'])
         ->name('api.tyres.show');
+
+    Route::get('/vehicles/{vehicle}/reading-monitoring-map', [TyreReadingMonitoringController::class, 'getVehicleTyreMapData'])
+        ->name('api.vehicles.reading-monitoring-map');
+
+    Route::get('/trailers/{trailer}/reading-monitoring-map', [TyreReadingMonitoringController::class, 'getTrailerTyreMapData'])
+        ->name('api.trailers.reading-monitoring-map');
 });
